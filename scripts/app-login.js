410da1941d8408996e8c13cca549c7130296b1f0
Michael <fatlycka@gmail.com>
2023-12-27 17:29:00 +0300

    pages for test

---
+++ scripts/app-login.js
@@ -0,0 +1,27 @@
+const formbtn = document.querySelector('.btn');
+// const inputs = document.querySelectorAll('.form-inputs');
+
+const formValues = document.querySelector('form');
+
+formbtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const fv = new FormData(formValues);
+    const obj = Object.fromEntries(fv);
+    const jsonData = JSON.stringify(obj);
+    const datafetch = await fetch('type url here', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+          },
+        body: jsonData
+     });
+
+    let result = await datafetch.json();
+
+    // Return -> main page
+        // window.location.assign('http://127.0.0.1:5000/');
+    
+
+    console.log(result)
+    console.log(jsonData)
+})
