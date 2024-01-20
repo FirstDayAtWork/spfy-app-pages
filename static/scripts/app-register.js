fff4a50c9d53ddd6f5d8e84b45e02414541a7d18
Michael <fatlycka@gmail.com>
2024-01-20 13:14:15 +0300

    replaced arrays with new Map objcts and fix regexp

--- static/scripts/app-register.js
+++ static/scripts/app-register.js
@@ -1,52 +1,47 @@
+"use strict"
 const formbtn = document.querySelector('.btn');
 const formInputs = document.querySelectorAll('.form-inputs');
-// http://localhost:2228/register
-// const inputs = document.querySelectorAll('.form-inputs');
 
 function keyDownEvents(){
     const REGEXP = regExpDelivery();
 
-    for(let i = 0; i < formInputs.length; i++){
-
-        formInputs[i].addEventListener('input', () => {
+    for(let elem of formInputs){
+        elem.addEventListener('input', () => {
             
             let user_err = document.createElement('small');
-            if(!REGEXP[i].test(formInputs[i].value)){
-                // console.log(formInputs[i].nextElementSibling.nodeName)
-                if(formInputs[i].nextElementSibling?.classList.contains('user-err')){
-                    formInputs[i].nextElementSibling.remove();
-                }
-                user_err.classList.add('user-err');
-                user_err.innerText = keydownMessages(formInputs[i])[i];
-                formInputs[i].style.border = '2px solid #b90909'
-                formInputs[i].after(user_err);
-                formbtn.disabled = true
-                
-            } else if(REGEXP[i].test(formInputs[i].value)){
-                if(formInputs[i].nextElementSibling?.classList.contains('user-err')){
-                    formInputs[i].nextElementSibling.remove();
-                }
-                    user_err.remove();
-                    formInputs[i].style.border = '2px solid #dddddd';
-                    formInputs[i].addEventListener('focus', () => formInputs[i].style.border = '2px solid #0941b9');
-                    formInputs[i].addEventListener('blur', () => formInputs[i].style.border = '2px solid #dddddd')
-                    formbtn.disabled = false
+            if(elem.nextElementSibling?.classList.contains('user-err')){
+                elem.nextElementSibling.remove();
             }
+
+            if(!REGEXP.get(elem.name).test(elem.value)){
+                user_err.classList.add('user-err');
+                user_err.innerText = keydownMessages(elem).get(elem.name);
+                elem.style.border = '2px solid #b90909'
+                elem.after(user_err);
+                formbtn.disabled = true
+            } else if(REGEXP.get(elem.name).test(elem.value)){
+                user_err.remove();
+                elem.style.border = '2px solid #dddddd';
+                elem.addEventListener('focus', () => elem.style.border = '2px solid #0941b9');
+                elem.addEventListener('blur', () => elem.style.border = '2px solid #dddddd')
+                formbtn.disabled = false
+            }
+
+           
         })
     }
-    
-    
+ 
 }
 
 keyDownEvents()
 
 function keydownMessages(inputName){
-    let arr = [];
-    let username = `${inputName.name.toUpperCase()} must be in range from 5 to 20 characters`;
-    let email = `${inputName.name.toUpperCase()} should look like this : example@gmail.com`;
-    let password = `Make sure ${inputName.name.toUpperCase()} it\'s at least 8 characters \n including: a lowercase letter, a uppercase letter, \n a number and special character`;
-    arr.push(username, email, password);
-    return arr
+    let errorMessages = new Map();
+    errorMessages.set('username', `${inputName.name.slice(0, 1).toUpperCase() + inputName.name.slice(1)} must be in range from 5 to 20 characters`)
+                .set('email', `${inputName.name.slice(0, 1).toUpperCase() + inputName.name.slice(1)} should look like this : example@gmail.com`)
+                .set('password', `Make sure ${inputName.name.slice(0, 1).toUpperCase() + inputName.name.slice(1)} it\'s at least 8 characters \n including: a lowercase letter, a uppercase letter, \n a number and special character`)
+
+    return errorMessages
 }
 
 
@@ -55,13 +50,13 @@ const formValues = document.querySelector('form');
 formbtn.addEventListener('click', async (e) => {
     e.preventDefault();
     const REGEXP = regExpDelivery();
-
+    
     const formInputs = document.querySelectorAll('.form-inputs');
     const userNameInput = document.querySelector('#username');
     const userEmailInput = document.querySelector('#email');
     const userPassInput = document.querySelector('#password');
 
-      let validation = preValidation(formInputs);
+      let validation = preValidation(formInputs, REGEXP);
 
       if(validation.every(el => el === false)){
         const fv = new FormData(formValues);
@@ -113,29 +108,33 @@ formbtn.addEventListener('click', async (e) => {
 
         
         // prevalidation logic
-        function preValidation(inputsData){
+        function preValidation(inputsData, REGEXP){
             let arr = [];
-            for(let i = 0; i < REGEXP.length; i++){
-                console.log([inputsData[i].value])
-                arr.push(!REGEXP[i].test([inputsData[i].value]))
-                if(!REGEXP[i].test([inputsData[i].value])){
+
+            for(let elem of inputsData){
+                
+                arr.push(!REGEXP.get(elem.name).test(elem.value))
+                if(!REGEXP.get(elem.name).test(elem.value)){
                     let user_err = document.createElement('small');
                     user_err.classList.add('user-err');
-                    user_err.innerText = `Invalid ${inputsData[i].name}`;
-                    inputsData[i].style.border = '2px solid #b90909'
-                    inputsData[i].after(user_err);
+                    user_err.innerText = `Invalid ${elem.name}`;
+                    elem.style.border = '2px solid #b90909'
+                    elem.after(user_err);
                     formbtn.disabled = true
 
-                    setTimeout(() => {
-                        user_err.remove();
-                        inputsData[i].style.border = '2px solid #dddddd';
-                        inputsData[i].addEventListener('focus', () => inputsData[i].style.border = '2px solid #0941b9');
-                        inputsData[i].addEventListener('blur', () => inputsData[i].style.border = '2px solid #dddddd')
-                        formbtn.disabled = false
-                    }, 5000);
+
+                        setTimeout(() => {
+                            user_err.remove();
+                            elem.style.border = '2px solid #dddddd';
+                            elem.addEventListener('focus', () => elem.style.border = '2px solid #0941b9');
+                            elem.addEventListener('blur', () => elem.style.border = '2px solid #dddddd')
+                            formbtn.disabled = false
+                        }, 5000);
+                    
                 }   
                 console.log('Invalid')
             }
+
             return arr
         }
     
@@ -148,32 +147,28 @@ formbtn.addEventListener('click', async (e) => {
 
     // Regexp Logic
     function regExpDelivery(){
+        const REGEXP = new Map();
 
-        const REGUSER = /^(?=[\w.-]{5,20}$)(?:[\d_.-]*[a-zA-Z]){3}[\w.-]*$/
+        REGEXP.set('username', /^(?=[\w.-]{5,20}$)(?:[\d_.-]*[a-zA-Z]){3}[\w.-]*$/)
             // ^ Start of string
             // (?=[\w.-]{5,20}$) Assert 5-20 of the allowed characters
             // (?:[\d_.-]*[a-zA-Z]){3} Match 3 times a-zA-Z followed by optional digits _ . -
             // [\w.-]* Match optional word chars or . or -
             // $ End of string
-
-        const REGMAIL = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/
+        .set('email', /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/)
             //  ^ - It is the start of the string.
             // [a-z0-9]+ - Any character between a to z and 0 to 9 at the start of the string.
             // @ - The string should contains ‘@’ character after some alphanumeric characters.
             // [a-z]+ - At least one character between a to z after the ‘@’ character in the string.
             // \. – Email should contain the dot followed by some characters followed by the ‘@’ character
             // [a-z]{2,3}$ - It should contain two or three alphabetical characters at the end of the string. The ‘$’ represents the end of the string.
-           
-        const REGPASS = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
+        .set('password', /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,72}$/)
             // old one : /^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])[a-zA-Z0-9]{6,}$/
             // At least 8 characters long
             // contains a lowercase letter
             // contains an uppercase letter
             // contains a digit
             // containes a special character
-        let arr = [REGUSER, REGMAIL, REGPASS];
-        
-            return arr
-        }
 
-console.log('force chrome to refresh cache -> Ctrl + F5')
+            return REGEXP
+        }
