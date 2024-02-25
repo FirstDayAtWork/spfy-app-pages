2413103479b94fc915284ef3894e4a0e48594fef
Michael <fatlycka@gmail.com>
2024-02-26 01:01:11 +0300

    change status code & add err msg & fix @media width treshold

--- static/scripts/app-login.js
+++ static/scripts/app-login.js
@@ -1,5 +1,5 @@
 const formbtn = document.querySelector('.btn');
-// const inputs = document.querySelectorAll('.form-inputs');
+const inputs = document.querySelectorAll('.form-inputs');
 
 const formValues = document.querySelector('form');
 
@@ -25,6 +25,22 @@ formbtn.addEventListener('click', async (e) => {
             console.log('repetitive login attempt was redirected');
             location.reload();
             return;
+        case 401:
+            const inputsContainer = document.querySelector('.inputs-container');
+            let user_err = document.createElement('small');
+                    user_err.classList.add('big-user-err');
+                    user_err.innerText = `Invalid username or password.`;
+                    // remove err if already exist
+                    if(inputsContainer.firstChild.classList?.contains('big-user-err')){
+                        inputsContainer.firstChild.remove();
+                    }
+                    inputsContainer.prepend(user_err);
+
+                    setTimeout(() => {
+                        user_err.remove();
+                    }, 5000);
+            console.log(datafetch.statusText, 'Invalid USERNAME or PASSWORD.')
+            return
         default:
             console.log(`unexpected status received: ${datafetch.status}`)
     }
